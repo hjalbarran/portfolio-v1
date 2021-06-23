af402322ec24d629ddf5c546e4062e5f0319d668
Humberto Albarrán <hjalbarran@gmail.com>
2021-06-22 20:18:19 -0400

    Added EmailJS to handled the sending email features of my portfolio, and first code working, just a copy of the docs of emailjs

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -56,29 +56,42 @@
             <div class="lg:w-8/12 md:w-7/12 sm:w-6/12 sm:text-center lg:text-justify px-4">
                 <h3 class="text-xl text-gray-400 font-semibold second-animation">Got a project? Drop me a line if you want to work together on something exciting. Big or small. Mobile or web.</h3>
                 <div class="container mx-auto pt-8">
-                    <div class="flex flex-wrap -m-2 third-animation">
+                    <!-- <div class="flex flex-wrap -m-2 third-animation">
                         <div class="p-2 w-1/2">
                             <div class="relative">
                                 <label for="name" class="leading-7 text-sm text-gray-600">Name</label>
-                                <input type="text" id="name" name="name" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                <input v-model="name" type="text" id="name" name="name" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                             </div>
                         </div>
                         <div class="p-2 w-1/2">
                             <div class="relative">
                                 <label for="email" class="leading-7 text-sm text-gray-600">Email</label>
-                                <input type="email" id="email" name="email" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                <input v-model="email" type="email" id="email" name="email" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                             </div>
                         </div>
                         <div class="p-2 w-full">
                             <div class="relative">
                                 <label for="message" class="leading-7 text-sm text-gray-600">Message</label>
-                                <textarea id="message" name="message" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+                                <textarea v-model="message" id="message" name="message" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
                             </div>
                         </div>
                         <div class="p-2 w-full">
-                            <button class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+                            <button class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg" @click="sendEmail($event)">Button</button>
                         </div>
-                    </div>
+                    </div> -->
+
+                    <div class="container">
+        <form class="contact-form" @submit.prevent="sendEmail">
+    <label>Name</label>
+    <input type="text" name="user_name">
+    <label>Email</label>
+    <input type="email" name="user_email">
+    <label>Message</label>
+    <textarea name="message"></textarea>
+    <input type="submit" value="Send">
+  </form>
+    </div>
+
                 </div>
             </div>
         </div>
@@ -87,8 +100,25 @@
 </template>
 
 <script>
+import emailjs from 'emailjs-com'
 export default {
-    
+  data () {
+    return {
+      name: null,
+      email: null, 
+      message: null
+    }
+  },
+  methods: {
+    sendEmail: (e) => {
+      emailjs.sendForm('service_bbxwl7k', 'template_9pgeflp', e.target, 'user_swYdziZ2vuZ4jvZHBYsl1')
+        .then((result) => {
+          console.log('SUCCESS!', result.status, result.text)
+        }, (error) => {
+          console.log('FAILED...', error)
+        })
+    }
+  }
 }
 </script>
 
