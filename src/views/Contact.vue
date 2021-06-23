43b54b7c4164e5e9cbda2cf321a0b90ef37fa0ed
Humberto Albarrán <hjalbarran@gmail.com>
2021-06-23 18:55:48 -0400

    Adding some accesibility enhancements to the contact form

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -55,43 +55,28 @@
             </div>
             <div class="lg:w-8/12 md:w-7/12 sm:w-6/12 sm:text-center lg:text-justify px-4">
                 <h3 class="text-xl text-gray-400 font-semibold second-animation">Got a project? Drop me a line if you want to work together on something exciting. Big or small. Mobile or web.</h3>
-                <div class="container mx-auto pt-8">
-                    <!-- <div class="flex flex-wrap -m-2 third-animation">
-                        <div class="p-2 w-1/2">
-                            <div class="relative">
-                                <label for="name" class="leading-7 text-sm text-gray-600">Name</label>
-                                <input v-model="name" type="text" id="name" name="name" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                <div class="container mx-auto pt-2">
+                    <form class="contact-form flex flex-wrap -m-2 third-animation" @submit.prevent="sendEmail">
+                        <fieldset class="container flex">
+                            <legend class="hidden">Name and email</legend>
+                            <div class="p-2 w-1/2">
+                                <label class="leading-7 text-sm text-gray-600">Name</label>
+                                <input type="text" name="name" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" required>
                             </div>
-                        </div>
-                        <div class="p-2 w-1/2">
-                            <div class="relative">
-                                <label for="email" class="leading-7 text-sm text-gray-600">Email</label>
-                                <input v-model="email" type="email" id="email" name="email" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                            <div class="p-2 w-1/2">
+                                <label class="leading-7 text-sm text-gray-600">Email</label>
+                                <input type="email" name="email" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" required>
                             </div>
-                        </div>
-                        <div class="p-2 w-full">
-                            <div class="relative">
-                                <label for="message" class="leading-7 text-sm text-gray-600">Message</label>
-                                <textarea v-model="message" id="message" name="message" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+                        </fieldset>
+                        <fieldset class="container flex">
+                            <legend class="hidden">Message</legend>
+                            <div class="p-2 w-full">
+                                <label class="leading-7 text-sm text-gray-600">Message</label>
+                                <textarea name="message" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out" required></textarea>
                             </div>
-                        </div>
-                        <div class="p-2 w-full">
-                            <button class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg" @click="sendEmail($event)">Button</button>
-                        </div>
-                    </div> -->
-
-                    <div class="container">
-        <form class="contact-form" @submit.prevent="sendEmail">
-    <label>Name</label>
-    <input type="text" name="user_name">
-    <label>Email</label>
-    <input type="email" name="user_email">
-    <label>Message</label>
-    <textarea name="message"></textarea>
-    <input type="submit" value="Send">
-  </form>
-    </div>
-
+                        </fieldset>
+                        <input type="submit" value="Send" class="flex mx-auto px-8 py-2 border border-transparent text-base font-medium rounded-md text-white bg-gray-600 md:py-4 md:text-lg md:px-10 hover:bg-gray-700 cursor-pointer">
+                    </form>
                 </div>
             </div>
         </div>
@@ -110,8 +95,8 @@ export default {
     }
   },
   methods: {
-    sendEmail: (e) => {
-      emailjs.sendForm('service_bbxwl7k', 'template_9pgeflp', e.target, 'user_swYdziZ2vuZ4jvZHBYsl1')
+    sendEmail: () => {
+      emailjs.sendForm('service_bbxwl7k', 'template_9pgeflp', '.contact-form', 'user_swYdziZ2vuZ4jvZHBYsl1')
         .then((result) => {
           console.log('SUCCESS!', result.status, result.text)
         }, (error) => {
