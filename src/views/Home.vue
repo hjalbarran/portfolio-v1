52e773f3735d9922d1d7cc06c992baef48e36720
Humberto Albarrán <hjalbarran@gmail.com>
2021-06-29 19:27:08 -0400

    enabled contact button from home/portfolio page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -65,8 +65,11 @@
     <section class="text-gray-600 body-font">
       <div class="container px-5 py-24 mx-auto">
         <div class="lg:w-2/3 flex flex-col sm:flex-row sm:items-center items-start mx-auto">
-          <h1 class="flex-grow sm:pr-16 text-2xl font-medium title-font text-gray-900" v-scrollanimation>Slow-carb next level shoindxgoitch ethical authentic, scenester sriracha forage.</h1>
-          <button class="flex-shrink-0 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg mt-10 sm:mt-0">Button</button>
+          <h1 class="flex-grow sm:pr-16 text-2xl font-medium title-font text-gray-900" v-scrollanimation>Got a project? Drop me a message!</h1>
+          <!-- <button class="flex-shrink-0 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg mt-10 sm:mt-0">Contact</button> -->
+          <router-link to="/contact" tag="a" class="w-auto flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-gray-600 hover:bg-gray-700 md:py-4 md:text-lg md:px-10 cursor-pointer">
+            Contact
+          </router-link>
         </div>
       </div>
     </section>
