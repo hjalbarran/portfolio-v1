ce658ad35a3a8a6c6d18e57b5e3b602fc76a38c4
Humberto Albarrán <hjalbarran@gmail.com>
2021-06-30 11:20:37 -0400

    Added some project images to the portfolio gallery

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -2,7 +2,7 @@
   <div class="p-6 m-2 mx-auto bg-white border rounded-lg shadow-xl lg:w-5/12 max-w-xl cursor-pointer" @click="projectDetailsURL(project)">
     <div class="flex flex-col items-start py-2 rounded-lg sm:flex-row">
       <div class="flex items-center justify-center w-full lg:justify-start lg:w-1/2">
-        <img src="https://dummyimage.com/200x200/F3F4F7/8693ac" alt="placeholder" class="rounded-lg">
+        <img :src="project.img" :alt="project.title" class="rounded-lg">
       </div>
       <div class="flex flex-col w-full text-blueGray-500 lg:ml-4" >
         <h2 class="mt-4 mb-1 text-base font-semibold tracking-widest text-black uppercase lg:mt-0 title-font">{{ project.title }}</h2>
